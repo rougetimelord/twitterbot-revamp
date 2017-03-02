5e8186f19b401bcb4e13ecebeb8f0a3fbfec6709
Rouge <sn.falconeye@yahoo.com>
2017-03-01 22:46:09 -0800

    Finish first testing version

--- twitter-bot/twitter_bot.py
+++ twitter-bot/twitter_bot.py
@@ -1,42 +1,59 @@
 import sys
-from TwitterAPI import TwitterAPI
+import tweepy
 
 consumer_key = "<FILL IN>"
 consumer_secret = "<FILL IN>"
 access_token_key = "<FILL IN>"
 access_token_secret = "<FILL IN>"
-api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
 
-tweetList = []
-twittersToRT = ["SkinDotTrade", "skinhub", "SteamAnalyst"]
-wordsToRT = ["giveaway", "contest", "enter", "rt"]
+twitters_to_rt = ["SkinDotTrade", "skinhub", "SteamAnalyst"]
+words_to_rt = ["giveaway", "contest", "enter", "rt"]
+
+num_entered = 0
 
 def retweet(id):
         success = False
+        fails = 0
         while success  == False:
-            try:
-                r = api.request("statuses/retweet", 
-                    {'id': id})
-                if response.getcode() == 200:
+            r = api.retweet(id);
+            if r.retweeted == "True":
+                success = True
+                num_entered += 1
+            else:
+                fails += 1
+                if fails >= 5:
+                    print("Really failed retweeting")
                     success = True
-            except Exception as e:
-                print(e)
-                time.sleep(5)
-        return "Done"
+                print("Failed to retweet")
+                time.sleep(60)
+
+def uni_norm(text):
+    return text.translate({ 0x2018:0x27, 0x2019:0x27, 0x201C:0x22, 0x201D:0x22,
+                            0xa0:0x20 })
 
 def getNewestTweets(user):
     tweets = []
     rt = []
-    for item in api.request("statuses/user_timeline", 
-        {'screen_name': user}, {'count': 10}):
-        tweets.append(item['id'], item['text'])
-
-    for tweet in tweets:
-        text = tweet[1].lower()
-        if any(x in text for x in wordsToRT):
-            rt.append(tweet[0])
+    for item in api.user_timeline(screen_name = user,count=20):
+        text = uni_norm(tweet.text).lower()
+        if any(x in text for x in words_to_rt):
+            rt.append(tweet.id_str)
+            if 'reply' in text:
+                print('Check @%s for a reply entry' % user)
 
     for id in rt:
         retweet(id)
-        
-    
+
+def startTweeting():
+    while 1 >= 0:
+        num_entered = 0
+        for user in twitters_to_rt:
+            getNewestTweets(user)
+        print("Entered %s contests on this run through, now sleeping for an hour" % num_entered)
+        time.sleep(3600)
+
+if __name__ == '__main__':
+    startTweeting()
